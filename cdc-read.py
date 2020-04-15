428a04dcb3306d92d54de539a6b06084f4e83bf1
ysafadi <ysafadi@umich.edu>
2020-04-15 16:26:57 -0400

    updated database

--- cdc-read.py
+++ cdc-read.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 import re
 import os
@@ -22,7 +23,14 @@ for state in info_dict["data"]:
     state_dict = {}
     state_dict["Jurisdiction"] = state.get("Jurisdiction", None)
     state_dict["Cases"] = state.get("Cases Reported", None)
-    state_dict["Transmission"] = state.get("Community Transmission�", None)
+    transmission = state.get("Community Transmission�", None)
+    if transmission == None:
+        state_dict["Transmission"] = transmission
+    elif transmission.find("Yes") == -1:
+        state_dict["Transmission"] = transmission
+    else:
+        trans = transmission.split(", ")
+        state_dict["Transmission"] = trans[1]
     state_list.append(state_dict)
 
 # set up connection to the database
@@ -30,7 +38,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/SI206_final_db.db')
 cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS US_Covid_19_Cases")
-cur.execute("CREATE TABLE US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, transmission TEXT)")
+cur.execute("CREATE TABLE US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, transmission TEXT, timestamp TEXT)")
 for i in (range(len(state_list) - 1)):
-    cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, transmission) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],state_list[i]["Transmission"]))
+    cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, transmission, timestamp) VALUES (?,?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],state_list[i]["Transmission"],datetime.now()))
 conn.commit()

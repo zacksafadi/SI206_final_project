0afc8f8327a6441111800b142b51ae5fa5e222c6
Rahul Shah <rahshah@umich.edu>
2020-04-19 17:07:59 -0400

    Call to OpenWeatherMap API
    Called and stored infromation on NYC with OpenWeatherMap

--- weather-read.py
+++ weather-read.py
@@ -1,1 +1,8 @@
 # starter for rahul to pull from weather API (had to add something to file so I could upload it to GitHub)
+import requests
+import json
+
+r = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=5128581&APPID=3c9071d80e11b58d16bd45c0ab95c7ad")
+j=r.json()
+print(j)
+

20463f9a50f52015c351420d32124bbb2a0f7b49
ysafadi <ysafadi@umich.edu>
2020-04-19 18:09:04 -0400

    separate tables for cases and transmission

--- cdc-read.py
+++ cdc-read.py
@@ -14,16 +14,16 @@ data = r.text
 soup = BeautifulSoup(data, "lxml")
 
 states = soup.find_all(class_="mb-3")
-request_url = "https://www.cdc.gov" + states[6].get("data-config-url")
+request_url = "https://www.cdc.gov/coronavirus/2019-ncov/json/us-cases-map-data.json"
 r1 = requests.get(request_url)
 info_dict = requests.get(request_url).json()
 #print(info_dict)
 state_list = []
-for state in info_dict["data"]:
+for state in info_dict:
     state_dict = {}
     state_dict["Jurisdiction"] = state.get("Jurisdiction", None)
     state_dict["Cases"] = state.get("Cases Reported", None)
-    transmission = state.get("Community Transmission�", None)
+    transmission = state.get("Community Transmission", None)
     if transmission == None:
         state_dict["Transmission"] = transmission
     elif transmission.find("Yes") == -1:
@@ -38,7 +38,10 @@ path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/SI206_final_db.db')
 cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS US_Covid_19_Cases")
-cur.execute("CREATE TABLE US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, transmission TEXT, timestamp TEXT)")
+cur.execute("DROP TABLE IF EXISTS US_Covid_19_Transmission")
+cur.execute("CREATE TABLE US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, timestamp TEXT)")
+cur.execute("CREATE TABLE US_Covid_19_Transmission (id INTEGER PRIMARY KEY, jurisdiction TEXT, transmission TEXT, timestamp TEXT)")
 for i in (range(len(state_list) - 1)):
-    cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, transmission, timestamp) VALUES (?,?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],state_list[i]["Transmission"],datetime.now()))
+    cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],datetime.now()))
+    cur.execute("INSERT INTO US_Covid_19_Transmission (id, jurisdiction, transmission, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Transmission"],datetime.now()))
 conn.commit()

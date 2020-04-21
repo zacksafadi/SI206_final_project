10af46a4097c93eef084acfdb957b8c17c60a8a7
ysafadi <ysafadi@umich.edu>
2020-04-20 21:11:58 -0400

    change in structure to data

--- cdc-read.py
+++ cdc-read.py
@@ -19,18 +19,33 @@ r1 = requests.get(request_url)
 info_dict = requests.get(request_url).json()
 #print(info_dict)
 state_list = []
+trans_list = []
+for i in range(4):
+    trans_list.append("N/A")
+    trans_list.append("Undetermined")
+    trans_list.append("defined area(s)")
+    trans_list.append("widepsread")
 for state in info_dict:
     state_dict = {}
     state_dict["Jurisdiction"] = state.get("Jurisdiction", None)
     state_dict["Cases"] = state.get("Cases Reported", None)
-    transmission = state.get("Community Transmission", None)
+    '''transmission = state.get("Community Transmission", None)
     if transmission == None:
         state_dict["Transmission"] = transmission
     elif transmission.find("Yes") == -1:
         state_dict["Transmission"] = transmission
     else:
         trans = transmission.split(", ")
-        state_dict["Transmission"] = trans[1]
+        state_dict["Transmission"] = trans[1]'''
+    
+    if state.get("Community Transmission", None) == "N/A":
+        state_dict["trans_id"] = 0
+    elif state.get("Community Transmission", None) == "Undetermined":
+        state_dict["trans_id"] = 1
+    elif state.get("Community Transmission", None) == "Yes, defined area(s)":
+        state_dict["trans_id"] = 2
+    else:
+        state_dict["trans_id"] = 3
     state_list.append(state_dict)
 
 
@@ -47,12 +62,25 @@ file.close()
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/SI206_final_db.db')
 cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS US_Covid_19_Cases")
-cur.execute("DROP TABLE IF EXISTS US_Covid_19_Transmission")
-cur.execute("CREATE TABLE US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, timestamp TEXT)")
-cur.execute("CREATE TABLE US_Covid_19_Transmission (id INTEGER PRIMARY KEY, jurisdiction TEXT, transmission TEXT, timestamp TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS US_Covid_19_Cases (id INTEGER PRIMARY KEY, jurisdiction TEXT, cases TEXT, transmission_id INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXITST US_Covid_19_Transmission (id INTEGER PRIMARY KEY, transmission TEXT)")
 for i in (range(len(state_list) - 1)):
-    cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],datetime.now()))
-    cur.execute("INSERT INTO US_Covid_19_Transmission (id, jurisdiction, transmission, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Transmission"],datetime.now()))
-conn.commit()
+    cur.execute("SELECT transmission FROM US_Covid_19_Transmission WHERE id = 1 LIMIT 1")
+    try:
+        # table is already filled, so do nothing
+        print("Transmission table already exists")
+    except:
+        for i in range(4):
+            cur.execute("INSERT INTO US_Covid_19_Transmission (id, transmission) VALUES (?,?)"(i, trans_list[i]))
 
+    cur.execute("SELECT cases FROM US_Covid_19_Cases WHERE jurisdiction = ? LIMIT 1",(state_list[i]["Jurisdiction"], ))
+    try:
+        # if already there, update the number of cases
+        cur.execute("UPDATE US_Covid_19_Cases SET (cases = ?, transmission_id = ?) WHERE name = ?"(state_list[i]["Cases"], state_list[i]["trans_id"], state_list[i]["Jurisdiction"]))
+        #cur.execute("UPDATE US_Covid_19_Cases SET transmission_id = ? WHERE name = ?"(state_list[i]["trans_id"], state_list[i]["Jurisdiction"]))
+    except:
+        # not there, so insert it
+        cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, transmission_id) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],state_list[i]["trans_id"])
+    #cur.execute("INSERT INTO US_Covid_19_Cases (id, jurisdiction, cases, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Cases"],datetime.now()))
+    #cur.execute("INSERT INTO US_Covid_19_Transmission (id, jurisdiction, transmission, timestamp) VALUES (?,?,?,?)",(i,state_list[i]["Jurisdiction"],state_list[i]["Transmission"],datetime.now()))
+conn.commit()

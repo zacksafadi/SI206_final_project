961a6bca0929360a515d728492b5fd224ca08b95
Rahul Shah <rahshah@umich.edu>
2020-04-26 18:36:14 -0400

    Graphs
    Finished

--- cases_temp_graph.py
+++ cases_temp_graph.py
@@ -2,24 +2,35 @@ import csv
 import matplotlib
 import matplotlib.pyplot as plt
 
-f = open("cases_data.txt", "r")
-data = f.readlines()
-f.close()
- #only interested in data[3] through data[5] for this graph
-temps = ['30º-49º', '50º-69º', '70º-89º']
-cases = []
-for line in data:
-    if line.find("Cloud") != -1 or line.find("Clear") != -1 or line.find("Rain") != -1:
-        continue
-    stats = line.split(",")
-    cases.append(float(stats[1][:-2]))
 
-width = 0.5
+def get_data(filename):
+    f = open(filename, "r")
+    data = f.readlines()
+    f.close()
+    return data
 
-fig, ax = plt.subplots()
-p1 = ax.bar(temps, cases, width, color='orange')
-ax.set(xlabel='Temperature Range (F)', ylabel='Avg Number of Covid Cases',
-       title='Average Number of Covid-19 Cases by Temperature Range')
-ax.grid()
 
-plt.show()
+def create_graph(temps, cases):
+    width = 0.5
+    fig, ax = plt.subplots()
+    p1 = ax.bar(temps, cases, width, color='orange')
+    ax.set(xlabel='Temperature Range (F)', ylabel='Avg Number of Covid Cases',
+        title='Average Number of Covid-19 Cases by Temperature Range')
+    ax.grid()
+    plt.show()  
+
+
+def main():
+    data = get_data("cases_data.txt")
+    #only interested in data[3] through data[5] for this graph
+    temps = ['30º-49º', '50º-69º', '70º-89º']
+    cases = []
+    for line in data:
+        if line.find("Cloud") != -1 or line.find("Clear") != -1 or line.find("Rain") != -1:
+            continue
+        stats = line.split(",")
+        cases.append(float(stats[1][:-2]))
+    create_graph(temps, cases)
+
+if __name__ == "__main__":
+    main()
---
+++ cases_weather_graph.py
@@ -0,0 +1,37 @@
+import csv
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def get_data(filename):
+    f = open(filename, "r")
+    data = f.readlines()
+    f.close()
+    return data
+
+
+def create_graph(weather, cases):
+    fig, ax = plt.subplots()
+    p1 = ax.bar(weather, cases, 0.5, color='orange')
+    ax.set(xlabel='Weather Patterns', ylabel='Avg Number of Covid Cases',
+        title='Average Number of Covid-19 Cases by Weather')
+    ax.grid()
+
+    plt.show()
+
+
+def main():
+    data = get_data("cases_data.txt")
+
+    weather = ['Clouds', 'Clear', 'Rain']
+    cases = []
+    for line in data:
+        if line.find("Cold") != -1 or line.find("Warm") != -1 or line.find("Hot") != -1:
+            continue
+        stats = line.split(",")
+        cases.append(float(stats[1][:-2]))
+
+    create_graph(weather, cases)
+
+if __name__ == "__main__":
+    main()

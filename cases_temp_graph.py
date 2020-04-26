dfc76052b8c6d20d2df3c83e8e353a74309af492
ysafadi <ysafadi@umich.edu>
2020-04-26 18:10:35 -0400

    graph customizations

--- cases_temp_graph.py
+++ cases_temp_graph.py
@@ -6,16 +6,20 @@ f = open("cases_data.txt", "r")
 data = f.readlines()
 f.close()
  #only interested in data[3] through data[5] for this graph
-temps = []
+temps = ['30º-49º', '50º-69º', '70º-89º']
 cases = []
 for line in data:
     if line.find("Cloud") != -1 or line.find("Clear") != -1 or line.find("Rain") != -1:
         continue
     stats = line.split(",")
-    temps.append(stats[0][1:])
     cases.append(float(stats[1][:-2]))
 
-plt.figure(figsize=(10,5))
-plt.bar(temps, cases)
-plt.title('Average Number of Covid-19 Cases by Temperature Range')
+width = 0.5
+
+fig, ax = plt.subplots()
+p1 = ax.bar(temps, cases, width, color='orange')
+ax.set(xlabel='Temperature Range (F)', ylabel='Avg Number of Covid Cases',
+       title='Average Number of Covid-19 Cases by Temperature Range')
+ax.grid()
+
 plt.show()
